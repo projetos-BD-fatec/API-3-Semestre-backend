29d40444ff2184e263331bf89fdb2d78bef9b7db
Luís Gustavo <graciano.luisgustavo@gmail.com>
2026-09-07 18:51:54 -0300

    [SCRUM-17] feat: configura conexão com banco Oracle e adiciona driver JDBC no Spring Boot

---
+++ src/main/java/br/com/bughunters/fusexflow/FusexflowBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.bughunters.fusexflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusexflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusexflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bughunters/fusexflow/FusexflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bughunters.fusexflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusexflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
